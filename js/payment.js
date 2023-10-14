07e0aeb536f3e0fd2cc7e22f872d6740be1d5311
Tiep-14 <conho3841@gmail.com>
2023-10-14 11:56:52 +0700

    a

--- js/payment.js
+++ js/payment.js
@@ -10,20 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: 'Gà', price: 8234 },
     ];
 
-    // Hiển thị sản phẩm trong giỏ hàng
-    function displayCart() {
-        cartItems.innerHTML = '';
-        let totalPrice = 0;
-        products.forEach(product => {
-            const item = document.createElement('li');
-            item.textContent = `${product.name} - ${product.price} VNĐ`;
-            cartItems.appendChild(item);
-            totalPrice += product.price;
-        });
-        totalAmount.textContent = totalPrice;
-    }
+    
 
-    displayCart();
 
     // Xử lý sự kiện khi người dùng gửi biểu mẫu thanh toán
     paymentForm.addEventListener('submit', function(event) {
@@ -37,3 +25,19 @@ document.addEventListener('DOMContentLoaded', function() {
         alert(`Thanh toán thành công!\nHọ tên: ${name}\nEmail: ${email}\nSố thẻ: ${card}`);
     });
 });
+
+// Lấy danh sách các phần tử có giá trị từ các thẻ <span> trong giỏ hàng
+const cartItems = document.querySelectorAll("#cart-items li .cart-item-info span:nth-child(2)");
+
+// Khởi tạo biến tổng tiền
+let totalAmount = 0;
+
+// Duyệt qua danh sách và tính tổng tiền
+cartItems.forEach(item => {
+  const price = parseInt(item.textContent); // Chuyển đổi giá từ chuỗi sang số
+  totalAmount += price;
+});
+
+// Cập nhật tổng tiền vào thẻ có id "total-amount"
+const totalAmountElement = document.getElementById("total-amount");
+totalAmountElement.textContent = totalAmount + " VNĐ";
